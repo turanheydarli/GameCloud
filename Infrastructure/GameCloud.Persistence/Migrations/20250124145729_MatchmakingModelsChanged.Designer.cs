0ec90ba67d339c2cda6259dba69e333045c35091
Turan Heydarli <turanheyder91@gmail.com>
2025-01-24 18:58:47 +0400

    mathcmaking migrated

--- Infrastructure/GameCloud.Persistence/Migrations/20250124145729_MatchmakingModelsChanged.Designer.cs
+++ Infrastructure/GameCloud.Persistence/Migrations/20250124145729_MatchmakingModelsChanged.Designer.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using GameCloud.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -13,9 +14,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GameCloud.Persistence.Migrations
 {
     [DbContext(typeof(GameCloudDbContext))]
-    partial class GameCloudDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250124145729_MatchmakingModelsChanged")]
+    partial class MatchmakingModelsChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
